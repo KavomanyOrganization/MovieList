e5cd95413282d431c7f832164017151f65713e2e
alonasaienko <saienko.alona@gmail.com>
2025-03-18 00:41:08 +0200

    add country controller

---
+++ MVC/Controllers/CountryController.cs
@@ -0,0 +1,70 @@
+using MVC.ViewModels;
+using MVC.Models;
+using MVC.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+
+namespace MVC.Controllers;
+
+public class CountryController : Controller{
+    protected readonly AppDbContext _context;
+
+    public CountryController(AppDbContext appDbContext){
+        _context = appDbContext;
+    }
+
+    public IActionResult Create(){
+        return View();
+    }
+
+    [Authorize(Roles = "Admin")]
+    [HttpPost]
+    public async Task<IActionResult> Create(CountryViewModel countryViewModel){
+        Country country = new Country(
+            countryViewModel.Name
+        );
+        _context.Countries.Add(country);
+        await _context.SaveChangesAsync();
+        //return RedirectToAction("ViewRating", "Movie");
+    }
+    [Authorize(Roles="Admin")]
+    public async Task<IActionResult> Delete(int id){
+        Country? country = await _context.Countries.FindAsync(id);
+        if (country == null)
+            return NotFound();
+
+        _context.Countries.Remove(country);
+        await _context.SaveChangesAsync();
+        //return RedirectToAction("ViewRating", "Movie");
+    }
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> Update(int id){
+        Country? country = await _context.Countries.FindAsync(id);
+        if (country == null)
+            return NotFound();
+            
+        var countryViewModel = new CountryViewModel
+        {
+            Name = country.Name
+        };
+        
+        ViewBag.CountryId = id;
+        return View(countryViewModel);
+    }
+
+    [Authorize(Roles = "Admin")]
+    [HttpPost]
+    public async Task<IActionResult> Update(int id, CountryViewModel countryViewModel){
+        Country? country = await _context.Countries.FindAsync(id);
+        if (country == null)
+            return NotFound();
+
+        country.Name = countryViewModel.Name;
+
+        _context.Countries.Update(country);
+        await _context.SaveChangesAsync();
+        //return RedirectToAction("ViewRating", "Movie");
+    }
+}
