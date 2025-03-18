ffed545db71bfdd3af23409b43a42cdd7718d171
sarcaazmm <halynamsgm@gmail.com>
2025-03-18 13:59:18 +0200

    Implement database seeding functionality

--- MVC/Models/UserMovie.cs
+++ MVC/Models/UserMovie.cs
@@ -10,15 +10,15 @@ public class UserMovie
     public int MovieId { get; set; }
     public Movie Movie { get; set; }
 
-    public int Raiting { get; set; }
+    public int Rating { get; set; }
     public bool IsWatched { get; set; }
 
     public UserMovie() { }
-    public UserMovie(string userId, int movieId, int raiting, bool isWatched)
+    public UserMovie(string userId, int movieId, int rating, bool isWatched)
     {
         UserId = userId;
         MovieId = movieId;
-        Raiting = raiting;
+        Rating = rating;
         IsWatched = isWatched;
     }
 }
