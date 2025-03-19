0a977cfad4046ad9c588d2975443d587062589a4
alonasaienko <saienko.alona@gmail.com>
2025-03-19 14:41:30 +0200

    genre pages

--- MVC/Controllers/GenreController.cs
+++ MVC/Controllers/GenreController.cs
@@ -15,20 +15,41 @@ public class GenreController : Controller{
         _context = appDbContext;
     }
 
-    public IActionResult Create(){
-        return View();
+    public async Task<IActionResult> GetAll()
+    {
+        var genres = await _context.Genres.OrderBy(g => g.Name).ToListAsync();
+        return View(genres);
     }
 
     [Authorize(Roles = "Admin")]
+    public IActionResult Create()
+    {
+        return View();
+    }
+    [Authorize(Roles = "Admin")]
     [HttpPost]
-    public async Task<IActionResult> Create(GenreViewModel genreViewModel){
-        Genre genre = new Genre
+    public async Task<IActionResult> Create(GenreViewModel genreViewModel)
+    {
+        if (ModelState.IsValid)
         {
-            Name = genreViewModel.Name
-        };
-        _context.Genres.Add(genre);
-        await _context.SaveChangesAsync();
-        return RedirectToAction("ViewGenre", "Genre");
+            var existingGenre = await _context.Genres.FirstOrDefaultAsync(g =>
+                g.Name.ToLower() == genreViewModel.Name.ToLower()
+            );
+
+            if (existingGenre != null)
+            {
+                ModelState.AddModelError("Name", "Already existing in List");
+                return View(genreViewModel);
+            }
+            Genre genre = new Genre { Name = genreViewModel.Name };
+
+            _context.Genres.Add(genre);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("GetAll", "Genre");
+        }
+
+        return View(genreViewModel);
     }
 
     [Authorize(Roles="Admin")]
@@ -39,7 +60,7 @@ public class GenreController : Controller{
 
         _context.Genres.Remove(genre);
         await _context.SaveChangesAsync();
-        return RedirectToAction("Index", "Home");
+        return RedirectToAction("GetAll", "Genre");
     }
 
     [Authorize(Roles = "Admin")]
@@ -68,6 +89,6 @@ public class GenreController : Controller{
 
         _context.Genres.Update(genre);
         await _context.SaveChangesAsync();
-        return RedirectToAction("Index", "Home");
+        return RedirectToAction("GetAll", "Genre");
     }
 }
--- MVC/ViewModels/CountryViewModel.cs
+++ MVC/ViewModels/CountryViewModel.cs
@@ -2,8 +2,8 @@ using System.ComponentModel.DataAnnotations;
 
 namespace MVC.ViewModels;
 
-public class CountryViewModel {
+public class CountryViewModel
+{
     [Required(ErrorMessage = "Country name is required")]
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
+    public string Name { get; set; }
 }
