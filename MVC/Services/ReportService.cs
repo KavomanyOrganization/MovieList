237ece629fc757b3103e57eb79705aba6eecb55e
alonasaienko <saienko.alona@gmail.com>
2025-04-01 09:00:10 +0300

    update report service

--- MVC/Services/ReportService.cs
+++ MVC/Services/ReportService.cs
@@ -3,21 +3,26 @@ using MVC.Data;
 using MVC.Models;
 using MVC.ViewModels;
 using MVC.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
 
 namespace MVC.Services;
 public class ReportService : IReportService
 {
     private readonly AppDbContext _context;
+    private readonly UserManager<User> _userManager;
 
-    public ReportService(AppDbContext context)
+    public ReportService(AppDbContext context, UserManager<User> userManager)
     {
         _context = context;
+        _userManager = userManager;
     }
 
     public async Task<List<Report>> GetAllReportsAsync()
     {
         return await _context.Reports
             .Include(r => r.Movie)
+            .Include(r => r.User)
             .OrderByDescending(r => r.CreationDate)
             .ToListAsync();
     }
@@ -34,16 +39,20 @@ public class ReportService : IReportService
                             .ToListAsync();
     }
 
-    public async Task<bool> CreateReportAsync(ReportViewModel reportViewModel)
+    public async Task<bool> CreateReportAsync(ReportViewModel reportViewModel, ClaimsPrincipal userPrincipal)
     {
         var movie = await _context.Movies.FindAsync(reportViewModel.MovieId);
         if (movie == null) return false;
+
+        var user = await _userManager.GetUserAsync(userPrincipal);
+        if (user == null) return false;
 
         var report = new Report
         {
             Comment = reportViewModel.Comment,
             CreationDate = DateTime.UtcNow,
-            MovieId = reportViewModel.MovieId
+            MovieId = reportViewModel.MovieId,
+            UserId = user.Id
         };
 
         _context.Reports.Add(report);
