9469128928349b011cacc4f48d90653d5d665512
alonasaienko <saienko.alona@gmail.com>
2025-04-01 08:53:44 +0300

    change report class

--- MVC/Models/Report.cs
+++ MVC/Models/Report.cs
@@ -10,11 +10,14 @@ public class Report
     public DateTime CreationDate { get; set; } = DateTime.UtcNow; 
 
     public int MovieId { get; set; }
-    public Movie? Movie { get; set; }    
+    public Movie? Movie { get; set; } 
+    public string UserId {get; set;} = "";
+    public User? User{ get; set;}   
     public Report(){}
-    public Report (string? comment, DateTime creationDate, int movieId){
+    public Report (string? comment, DateTime creationDate, int movieId, string userId){
         Comment = comment;
         CreationDate = creationDate;
         MovieId = movieId; 
+        UserId = userId;
     }
 }
