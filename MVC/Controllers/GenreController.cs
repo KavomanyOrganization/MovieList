8fd7b554c464cb3be0eecbbbb5e1b880e2415593
alonasaienko <saienko.alona@gmail.com>
2025-04-21 11:15:00 +0300

    add search view to genre

--- MVC/Controllers/GenreController.cs
+++ MVC/Controllers/GenreController.cs
@@ -86,6 +86,13 @@ namespace MVC.Controllers
 
             return RedirectToAction("GetAll");
         }
+        [HttpGet]
+        public async Task<IActionResult> Search(string searchTerm)
+        {
+            var genres = await _genreService.SearchGenresAsync(searchTerm);
+            ViewBag.Genres = genres.OrderByDescending(g => g.Name).ToList();
+            return View("GetAll", new GenreViewModel());
+        }
 
     }
 }
