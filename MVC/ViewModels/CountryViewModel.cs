9cea16f57233ef1c1ef0cfda79d36f60d8fd714c
alonasaienko <saienko.alona@gmail.com>
2025-05-12 20:09:36 +0300

    update styles for pages

--- MVC/ViewModels/CountryViewModel.cs
+++ MVC/ViewModels/CountryViewModel.cs
@@ -4,6 +4,7 @@ namespace MVC.ViewModels;
 
 public class CountryViewModel
 {
+    public int Id { get; set; }
     [Required(ErrorMessage = "Country name is required")]
     public string Name { get; set; } = null!;
 }
