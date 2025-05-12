73e3722031a00f32f50a8234e5eefe46cc7db53a
alonasaienko <saienko.alona@gmail.com>
2025-05-12 19:44:36 +0300

    update getall udate pages for genres

--- MVC/ViewModels/GenreViewModel.cs
+++ MVC/ViewModels/GenreViewModel.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations;
 namespace MVC.ViewModels;
 
 public class GenreViewModel {
+    public int Id { get; set; }
     [Required(ErrorMessage = "Genre name is required")]
     public string Name { get; set; } = null!;
 }
